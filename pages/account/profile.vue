b0fb372c7f4eab49538ea055e6553e56f14ed6da
Mohammad Rezazadeh <mrprocs@gmail.com>
2022-02-01 21:12:36 +0330

    Create profile page

---
+++ pages/account/profile.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="col-sm-4 p-3">
+  <label class="my-2">Email:</label>
+  <b-form-input type="text"  :value="this.$auth.user.email"  readonly  />
+  <label class="my-2">First Name:</label>
+  <b-form-input type="text"  :value="this.$auth.user.first_name"  readonly  />
+  <label class="my-2">Last Name:</label>
+  <b-form-input type="text"  :value="this.$auth.user.last_name"  readonly  />
+</div>
+</template>
+
+<script>
+export default {
+  name: "profile"
+}
+</script>
+
+<style scoped>
+
+</style>
