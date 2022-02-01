07c6c0c720431db5b2c01cc68c32db7b6649d446
Mohammad Rezazadeh <mrprocs@gmail.com>
2022-02-01 21:10:31 +0330

    First commit

---
+++ nuxt.config.js
@@ -0,0 +1,90 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'success-roadmap %s',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''},
+      {name: 'format-detection', content: 'telephone=no'}
+    ],
+    link: [
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  globals: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    'bootstrap-vue/nuxt',
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy',
+    '@nuxtjs/auth-next',
+
+  ],
+
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // baseURL: 'http://127.0.0.1:8000/',
+    proxy: true
+  },
+  proxy: {
+    '/API': {
+      target: 'http://127.0.0.1:8000/',
+      pathRewrite: {'^/API': ''},
+      changeOrigin: true
+    }
+  },
+
+  auth: {
+    strategies: {
+      local: {
+        token: {
+          property: 'key',
+          global: true,
+          required: true,
+          type: 'token'
+        },
+        user: {
+          property: '',
+          autoFetch: true
+        },
+        // cookie: {
+        //   cookie: {
+        //     // (optional) If set, we check this cookie existence for loggedIn check
+        //     name: 'csrftoken',
+        //   },
+        // },
+        endpoints: {
+          login: {url: '/API/accounts/api/login/', method: 'post'  },
+          logout: {url: '/API/accounts/api/logout/', method: 'post'},
+          user: {url: '/API/accounts/api/user/', method: 'get' }
+        }
+      }
+    }
+
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {}
+}
