5d5adc047ea85d416d91d07305defa34a56ad4f4
Mohammad Rezazadeh <mrprocs@gmail.com>
2022-02-03 21:58:46 +0330

    Create verify email page

---
+++ pages/account/confirm/email/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="col-sm-4 offset-4">
+    <h2>Verify email address</h2>
+    <h5>To verify email address please click on the bellow button.</h5>
+
+    <b-button @click="verifyEmail">Verify</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  middleware: "auth",
+  auth: "guest",
+  methods: {
+    async verifyEmail() {
+      this.error = ''
+      try {
+        let response = await this.$axios.post(
+          'endpoint/accounts/api/registration/verify-email/',
+          {
+            key: this.$route.query.key
+          }
+        )
+        this.$router.push('/done?msg=Your email address id verified.&url=account/login&button=Login')
+      } catch (err) {
+        this.error = err.response.data
+        console.log(err)
+      }
+
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
