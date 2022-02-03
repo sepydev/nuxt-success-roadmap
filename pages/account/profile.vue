5b39d340bb33b8ca3149e7d17ae2468cbe031174
Mohammad Rezazadeh <mrprocs@gmail.com>
2022-02-03 16:46:50 +0330

    Add feature to edit user's first name and last name

--- pages/account/profile.vue
+++ pages/account/profile.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="col-sm-4 offset-md-4">
     <h2>User Profile</h2>
-    <b-form>
+    <b-form @submit.prevent="saveUserData">
       <label class="my-2">Email:</label>
       <b-form-input type="text" :value="this.$auth.user.email" readonly/>
       <label class="my-2">First Name:</label>
-      <b-form-input type="text" :value="this.$auth.user.first_name" required/>
+      <b-form-input type="text" v-model="user.first_name" required/>
       <label class="my-2">Last Name:</label>
-      <b-form-input type="text" :value="this.$auth.user.last_name" required/>
+      <b-form-input type="text" v-model="user.last_name" required/>
       <br/>
-      <b-button type="submit" >Save Changes</b-button>
+      <b-button type="submit">Save Changes</b-button>
       <b-button to="/account/change-password" class="btn-danger">Change Password</b-button>
     </b-form>
   </div>
@@ -19,6 +19,26 @@
 export default {
   name: "profile",
   middleware: "auth",
+  data() {
+    return {
+      user: {
+        first_name: this.$auth.user.first_name,
+        last_name: this.$auth.user.last_name
+      }
+    }
+  }
+  ,
+  methods: {
+    async saveUserData() {
+      try {
+        let response = await this.$axios.put('endpoint/accounts/api/user/',
+          this.user)
+        this.$auth.fetchUser()
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
 }
 </script>
 
